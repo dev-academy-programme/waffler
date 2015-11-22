2b7b98ed94e117626b2b72df3df715df9d3a42a8
peterjacobson <peterjacobson.nz@gmail.com>
2015-11-22 17:28:39 +1300

    setup for promises

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # push-waffle
-Load a json of issues into a waffle board
+Load a json of issues into a git repo
+
+## setting up cohort curriculum
+
+1. globally install push-waffle `npm install -g push-waffle`
+1. Create cohort repo e.g. *moa-2016*
+2. Add `students.json` to repo, e.g.
+  ```
+  {
+    student_github_names: [
+      'peterjacobson',
+      'pietguersen',
+      'jamanius',
+      'joshuavial'
+    ]
+  }
+  ```
+3. Clone/navigate to [curriculum-private](https://github.com/dev-academy-phase0/curriculum-private)
+4. `git pull` to get latest changes
+5. run push-waffle `push-waffle`
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require("fs")
+var prompt = require("prompt")
 var GitHubApi = require('github')
 var github = new GitHubApi({
   version: "3.0.0",
@@ -6,18 +7,63 @@ var github = new GitHubApi({
   protocol: "https",
 })
 
+// get params
+
+  // authenticate github
+    // add sprint labels if needed
+
+  // read&parse #-assignments.json
+    // read&parse assignment md files]
+      // build issues from assignments
+
+    //  post issues to github
+
+
 github.authenticate({
   type: "basic", 
   username: process.env.GITHUB_USER,
-  password: process.env.GITHUB_PASSWORD
+  password: process.env.GITHUBPASSWORD
 })
 
-var sprintAssignmentPath = './' + process.argv[2]
-fs.readFile(sprintAssignmentPath, function(err, data) {
-  if (err) { throw err }
-  assignmentPaths = JSON.parse(data)
-  console.log(assignmentPaths);
-})
+userParams = getParams()
+console.log(userParams);
+
+function getParams(){
+  var sprintAssignmentPath = './' + process.argv[2]
+  var promptResults
+  var schema = {
+    properties: {
+      cohort: {
+        message: "cohort's git repo, e.g. moa-2016",
+        required: true
+      },
+      githubUsername: {
+        required: true
+      },
+      githubPassword: {
+        required: true,
+        hidden: true
+      }
+    }
+  }
+  prompt.start()
+  prompt.get(schema, function(err, results) {
+    if (err) { throw err }
+    promptResults = results
+  })
+  return {
+    sprintAssignmentPath: sprintAssignmentPath,
+    githubCohortRepo: promptResults.cohort,
+    githubUsername: promptResults.githubUsername,
+    githubPassword: promptResults.password,
+  }
+}
+
+// fs.readFile(sprintAssignmentPath, function(err, data) {
+//   if (err) { throw err }
+//   assignmentPaths = JSON.parse(data)
+//   console.log(assignmentPaths);
+// })
 // console.log(sprintAssignments);
 
 var assignments = [
@@ -36,10 +82,10 @@ var githubUserOrOrg = 'dev-academy-phase0'
 var cohortRepo = 'kakapo-2016-backlog'
 
 var issues = compileIssuesObject(githubUserOrOrg, cohortRepo, assignments, moaStudents)
-// createSprintLabels(githubUserOrOrg, cohortRepo)
+// postSprintLabels(githubUserOrOrg, cohortRepo)
 // postIssues(issues)
 
-function createSprintLabels(githubUserOrOrg, cohortRepo) {
+function postSprintLabels(githubUserOrOrg, cohortRepo) {
   var colors = ['ffb366', 'ffd966', 'ffff66', 'd9ff66', '8cff66', '66ff8c', '66ffd9', '66d9ff', '668cff']
   numOfWeeks = 9
   for (var week = 1; week <= numOfWeeks; week++) {
