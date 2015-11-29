a3ce3bd9e54a6f65397a850087ba183b19e618ab
peterjacobson <peterjacobson.nz@gmail.com>
2015-11-29 23:11:01 +1300

    fix label index+1

--- create-issue-labels.js
+++ create-issue-labels.js
@@ -2,14 +2,15 @@ module.exports = function(cohortRepo, github) {
   
   var githubUserOrOrg = 'dev-academy-programme'
   var issueColors = ['ffb366', 'ffd966', 'ffff66', 'd9ff66', '8cff66', '66ff8c', '66ffd9', '66d9ff', '668cff']
-  var numOfWeeks = 9
+  var numOfSprints = 9
 
-  for (var week = 1; week <= numOfWeeks; week++) {
+  for (var sprint = 1; sprint <= numOfSprints; sprint++) {
     github.issues.createLabel({
       user: githubUserOrOrg,
       repo: cohortRepo,
-      name: 'sprint-' + week,
-      color: issueColors[week]
+      name: 'sprint-' + sprint,
+      color: issueColors[sprint-1]
     })
+    console.log('labelled sprint-' + sprint);
   };
 }
