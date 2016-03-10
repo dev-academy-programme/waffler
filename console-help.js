1d0de8b18110cb927e80f667518b99384f38bf76
peterjacobson <peterjacobson.nz@gmail.com>
2016-03-11 12:35:30 +1300

    document setup, functionality and use

--- console-help.js
+++ console-help.js
@@ -1,30 +1,14 @@
 module.exports = function() {
   console.log(`
 
-Options:
-  push      push a sprint of assignments to a cohort
-  label     add sprint-1 to 9 labels to cohort repo
-
-
-Examples:
-  send assignments:
-        $ waffle push <sprint#> <cohort>
-    e.g.$ waffle push 1 moa-2016
-
-
-  create sprint labels for cohort repo (needed once per cohort):
-        $ waffle label <cohort>
-    e.g.$ waffle label moa-2016
-
-  export GITHUB_USERNAME and GITHUB_PASSWORD from shell 
+  1. export GITHUB_USERNAME and GITHUB_PASSWORD from shell 
   to avoid github details prompt
 
+  2. 'npm install -g waffler'
 
-Detailled usage:
-  1. globally install assignment-pusher 'npm install -g assignment-pusher'
-  1. Create cohort repo e.g. 'moa-2016' in the 'dev-academy-programme' org
-  2. Add 'students.json' to root of cohort repo, e.g.
-    '
+  3. Create cohort repo e.g. 'moa-2016' in the 'dev-academy-programme' org
+
+  4. Add 'students.json' to root of cohort repo, e.g.
     {
       "studentGithubNames": [
         "peterjacobson",
@@ -33,10 +17,34 @@ Detailled usage:
         "joshuavial"
       ]
     }
-    '
-  3. Clone/navigate to [curriculum-private](https://github.com/dev-academy-programme/curriculum-private)
-  4. 'git pull' to get latest changes
-  5. run as per instructions above
-  
+
+  5. Clone & navigate to [curriculum-private](https://github.com/dev-academy-programme/curriculum-private) *the waffler gets list of assignments from github.com, but scrapes local file system for assignment content. don't ask. needs rebuild*
+
+  6. 'git pull' to get latest changes
+
+  7. run as below:
+
+  Options:
+    push      push a sprint of assignments to a cohort
+    label     add sprint-1 to 9 labels to cohort repo
+    patch     execute find & replace on an assignment in a repo
+
+  Examples:
+    
+    add sprint labels to cohort repo (needed once per cohort):
+          $ waffle label <cohort>
+      e.g.$ waffle label moa-2016
+
+    push assignments:
+          $ waffle push <sprint#> <cohort> <members>
+      e.g.$ waffle push 1 moa-2016 -  
+      e.g.$ waffle push 3 moa-2016 peterjacobson
+      <members>: '-'= all users listed in students.json in root of cohort repo OR 'githubusername' 
+
+    patch assignments:
+          $ waffle patch <cohort> <assignment number> <string to find> <string to replace>
+      e.g.$ waffle patch moa-2010 4.2 "string to find" "string to replace"
+
+
   `);
 }
